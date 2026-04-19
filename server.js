50473f8af6908910ad7600f1798d44beef2e2b80
mapetoxavier-star <mapetoxavier@gmail.com>
2026-04-19 11:47:19 +0100

    Create server.js

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+// Route test
+app.get('/', (req, res) => {
+  res.send('GodLive Backend API 🚀');
+});
+
+// Login simulation
+app.post('/api/login', (req, res) => {
+  const { telephone, password } = req.body;
+
+  if (telephone === "0990000000" && password === "1234") {
+    return res.json({
+      message: "Connexion réussie",
+      user: {
+        id: 1,
+        nom: "Agent Test",
+        role: "agent"
+      }
+    });
+  }
+
+  res.status(401).json({ message: "Identifiants invalides" });
+});
+
+module.exports = app;
